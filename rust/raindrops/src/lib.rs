3660333173e338c76d8bb8107378ef9675d576fd
Matthew Perez <matt@mperez.io>
2020-08-26 14:10:08 -0500

    Complete beer-song and nth-prime

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut tunes = HashMap::new();
+    let mut melody = vec![];
+
+    tunes.insert(3, "Pling".to_string());
+    tunes.insert(5, "Plang".to_string());
+    tunes.insert(7, "Plong".to_string());
+    
+    for (num, sound) in &tunes {
+        if n % num == 0 {
+            melody.push(sound.clone());
+        } else { 
+            continue;
+        };
+    };
+
+    if melody.is_empty() {
+        return n.to_string()
+    } else {return melody.join("")}
+
+    
 }
